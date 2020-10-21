96f1648becfe615145ac09c8d1d610ad5aa5223c
Thomas Tang <tni711@gmail.com>
2020-10-21 17:40:19 -0400

    Baseline configuration tested ok

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Webpack4 React Development Lab!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
